8ce2fbcfae4be81dd92d78fc9d2f7dfee4724828
Hoang Tu <tuvo@pandavietnamsoftware.com>
2022-02-14 11:23:57 +0700

    test

---
+++ src/Core/Commands/InstallCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kcms\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command{
+
+    protected $signature = 'kcms:install';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $this->info("kcms:install");
+    }
+}
