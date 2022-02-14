37aa31091b518ae8da0984e2b341599a038e78d1
Hoang Tu <tuvo@pandavietnamsoftware.com>
2022-02-14 11:05:04 +0700

    init

---
+++ src/Core/Providers/CoreServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kms\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider{
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    
+}
